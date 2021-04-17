d357c0899adf1dc3356a0b9080bb19aaec0f0f36
Ethan Kamus <ethanjpkamus@csu.fullerton.edu>
2021-04-17 14:07:21 -0700

    WIP

--- parser.cpp
+++ parser.cpp
@@ -11,10 +11,11 @@ using std::endl;
 void parser(vector<sig_item> v)
 {
 	//print out token and lexeme here
+
 	for(int i = 0; i < v.size(); ++i)
 	{
 		// cout << setw(10) << v[i].token  << " | " << v[i].lexeme << endl;
-		cout << "Token: " << v[i].token << " Lexeme: " << v[i].lexeme << endl;
+		cout << v[i].lexeme << " ";
 	}
 	cout << endl;
 	//print out associated rules here
@@ -25,24 +26,62 @@ void parser(vector<sig_item> v)
  * Declarative Production Rules
  */
 
-//<Statement> -> <Declarative> 
+//<Statement> -> <Declarative>
+bool isStatement()
+{
+	cout << "<Statement> -> <Declarative>" << endl;
+	//return isDeclarative();
+} 
 //<Declarative> -> <Type> <ID> 
+bool isDeclarative()
+{
+	cout << "<Declarative> -> <Type> <ID>" << endl;
+	//isType()
+	//isID()
+}
 //<Type> -> bool | float | int
+bool isType()
+{
+	cout << "<Type> -> bool | float | int" << endl;
+}
 
 /*
  * Arithmetic Production Rules
  */
 
 //<Expression> -> <Expression> + <Term> | <Expression> - <Term> | <Term> 
+bool isExpression()
+{
+	cout << "<Expression> -> <Expression> + <Term> | <Expression> - <Term> | <Term>" << endl;
+}
 //<Term> -> <Term> * <Factor> | <Term> / <Factor> | <Factor>
+bool isTerm()
+{
+	cout << "<Term> -> <Term> * <Factor> | <Term> / <Factor> | <Factor>" << endl;
+}
 //<Factor> -> ( <Expression> ) | <ID> | <Num>
+bool isFactor()
+{
+	cout << "<Factor> -> ( <Expression> ) | <ID> | <Num>" << endl;
+}
 //<ID> -> id
+bool isID()
+{
+	cout << "<ID> -> id" << endl;
+}
 
 /*
  * Assignment Production Rules
  */
 
 //<Statement> -> <Assign>
+bool isStatement()
+{
+	cout << "<Statement> -> <Assign>" << endl;
+}
 //<Assign> -> <ID> = <Expression>;
-
+bool isAssign()
+{
+	cout << "<Assign> -> <ID> = <Expression>;" << endl;
+}
 
